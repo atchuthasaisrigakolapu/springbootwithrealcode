a1988d375e2acfac9a07b1bedc95a6084e89e1dd
atchuthasaisrigakolapu <atchuthasaisrigakolapu@gmail.com>
2022-04-28 11:55:04 +0530

    testpurpose

---
+++ src/main/java/com/example/springBootWithRealcode/SpringBootWithRealcodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springBootWithRealcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithRealcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithRealcodeApplication.class, args);
+	}
+
+}
