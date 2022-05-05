d5c9a496d3f8fdf1bccbbd898caf39ffa325507b
atchuthasaisrigakolapu <atchuthasaisrigakolapu@gmail.com>
2022-05-05 17:42:55 +0530

    Added testing3

--- src/main/java/com/example/springBootWithRealcode/SpringBootWithRealcodeApplication.java
+++ src/main/java/com/example/springBootWithRealcode/SpringBootWithRealcodeApplication.java
@@ -3,12 +3,16 @@ package com.example.springBootWithRealcode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
 public class SpringBootWithRealcodeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWithRealcodeApplication.class, args);
 		System.out.print("testing purpose committed");
+		Map<String,String> map = new HashMap<>();
 	}
 
 }
