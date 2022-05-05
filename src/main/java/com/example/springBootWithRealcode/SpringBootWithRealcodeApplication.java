3b9debe05d9283b04629ff2cbfb5cef152536982
atchuthasaisrigakolapu <atchuthasaisrigakolapu@gmail.com>
2022-05-05 17:41:34 +0530

    Committed testing2

--- src/main/java/com/example/springBootWithRealcode/SpringBootWithRealcodeApplication.java
+++ src/main/java/com/example/springBootWithRealcode/SpringBootWithRealcodeApplication.java
@@ -3,12 +3,16 @@ package com.example.springBootWithRealcode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class SpringBootWithRealcodeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWithRealcodeApplication.class, args);
 		System.out.print("testing purpose committed");
+		List<String> list =  new ArrayList();
 	}
 
 }
